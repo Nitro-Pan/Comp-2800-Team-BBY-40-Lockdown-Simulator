c2be3ee6819a336c5250ced4e10b67a654ccc2be
Tate Smith <tate.smith.4242@gmail.com>
2020-04-28 17:58:13 -0700

    added dialogue box system

---
+++ Lockdown Simulator/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public Text nameText;
+    public Text dialogueText;
+    public Animator animator;
+
+    private Queue<string> sentences;
+
+    Coroutine sentenceBox;
+
+    // Start is called before the first frame update
+    void Start() {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue) {
+        animator.SetBool("isOpen", true);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences) {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence() {
+        if (sentences.Count == 0) {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+
+        if (sentenceBox != null)
+            StopCoroutine(sentenceBox);
+
+        sentenceBox = StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence) {
+        dialogueText.text = "";
+        foreach (char c in sentence.ToCharArray()) {
+            dialogueText.text += c;
+            yield return null;
+        }
+    }
+
+    public void EndDialogue() {
+        animator.SetBool("isOpen", false);
+    }
+}
