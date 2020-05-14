5ac60113c760a1df26e9541f379c1eba08fea561
Tate Smith <tate.smith.4242@gmail.com>
2020-05-13 17:27:43 -0700

    added win and lose conditions with some nifty animations

--- Lockdown Simulator/Assets/Scripts/ResidentManager.cs
+++ Lockdown Simulator/Assets/Scripts/ResidentManager.cs
@@ -33,8 +33,6 @@ public class ResidentManager : MonoBehaviour {
 
     public void EndDay() {
         RandomEvent re = RandomEvent.CreateSeededEvent(fHappiness, fInfectionRate);
-        dm.StartDialogue(re.dialogue);
-        nDay += 1;
         fHappiness = Mathf.Clamp(fHappiness + re.fHappinessGain, 0, 100f);
         fInfectionRate = Mathf.Clamp(fInfectionRate + re.fInfectionRate, 0, 100f);
         //if this is above the top, there is no chance of a random event chaning the outcome
@@ -46,6 +44,8 @@ public class ResidentManager : MonoBehaviour {
             WinGame();
             return;
         }
+        dm.StartDialogue(re.dialogue);
+        nDay += 1;
         textDay.text = nDay.ToString();
         textHappiness.text = "Happiness: " + fHappiness + " / " + fTotalHappiness;
         textResident.text = "Infected: " + fInfectionRate + "%";
@@ -57,11 +57,42 @@ public class ResidentManager : MonoBehaviour {
     }
 
     private void EndGame() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        CanvasGroup ui = GameObject.FindGameObjectWithTag("Game UI").GetComponent<CanvasGroup>();
+        StartCoroutine(FadeUI(ui, '-'));
+        CreateUI("Screens/Game End");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    private void WinGame() {
+        CanvasGroup ui = GameObject.FindGameObjectWithTag("Game UI").GetComponent<CanvasGroup>();
+        StartCoroutine(FadeUI(ui, '-'));
+        CreateUI("Screens/Game Win");
     }
 
-    private void WinGame() {
-        textResident.text = "Game Won!";
+    IEnumerator FadeUI(CanvasGroup ui, char op) {
+        float fFadeSpeed = 0.05f;
+        switch (op) {
+            case '-':
+                while (ui.alpha > 0) {
+                    Debug.Log("Fade UI out");
+                    ui.alpha -= fFadeSpeed;
+                    yield return null;
+                }
+                break;
+            case '+':
+                while (ui.alpha < 1) {
+                    Debug.Log("Fade UI in");
+                    ui.alpha += fFadeSpeed;
+                    yield return null;
+                }
+                break;
+        }
+    }
+
+    private void CreateUI(string sFilePath) {
+        GameObject goScreen = Resources.Load<GameObject>(sFilePath);
+        goScreen.GetComponent<CanvasGroup>().alpha = 0;
+        GameObject goEditorObject = Instantiate(goScreen, new Vector3(0, 0, -3), Quaternion.identity);
+        StartCoroutine(FadeUI(goEditorObject.GetComponent<CanvasGroup>(), '+'));
     }
 
     public void IncreaseInfection() {
