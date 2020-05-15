e32da7c75cbe612fe08c55ae67e61a722fbd1949
Tate Smith <tate.smith.4242@gmail.com>
2020-05-14 17:06:33 -0700

    fixed up end conditions

--- Lockdown Simulator/Assets/Scripts/ResidentManager.cs
+++ Lockdown Simulator/Assets/Scripts/ResidentManager.cs
@@ -37,6 +37,7 @@ public class ResidentManager : MonoBehaviour {
         fInfectionRate = Mathf.Clamp(fInfectionRate + re.fInfectionRate, 0, 100f);
         //if this is above the top, there is no chance of a random event chaning the outcome
         //if it's below, you might get screwed over last minute. Strategy.
+        dm.StartDialogue(re.dialogue);
         if (fInfectionRate >= fTotalInfectionRate) {
             EndGame();
             return;
@@ -44,7 +45,6 @@ public class ResidentManager : MonoBehaviour {
             WinGame();
             return;
         }
-        dm.StartDialogue(re.dialogue);
         nDay += 1;
         textDay.text = nDay.ToString();
         textHappiness.text = "Happiness: " + fHappiness + " / " + fTotalHappiness;
@@ -60,7 +60,6 @@ public class ResidentManager : MonoBehaviour {
         CanvasGroup ui = GameObject.FindGameObjectWithTag("Game UI").GetComponent<CanvasGroup>();
         StartCoroutine(FadeUI(ui, '-'));
         CreateUI("Screens/Game End");
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     private void WinGame() {
         CanvasGroup ui = GameObject.FindGameObjectWithTag("Game UI").GetComponent<CanvasGroup>();
@@ -69,18 +68,21 @@ public class ResidentManager : MonoBehaviour {
     }
 
     IEnumerator FadeUI(CanvasGroup ui, char op) {
+        while (dm.bDialogueOpen) {
+            yield return null;
+        }
+        ui.blocksRaycasts = true;
+        ui.interactable = true;
         float fFadeSpeed = 0.05f;
         switch (op) {
             case '-':
                 while (ui.alpha > 0) {
-                    Debug.Log("Fade UI out");
                     ui.alpha -= fFadeSpeed;
                     yield return null;
                 }
                 break;
             case '+':
                 while (ui.alpha < 1) {
-                    Debug.Log("Fade UI in");
                     ui.alpha += fFadeSpeed;
                     yield return null;
                 }
@@ -90,7 +92,13 @@ public class ResidentManager : MonoBehaviour {
 
     private void CreateUI(string sFilePath) {
         GameObject goScreen = Resources.Load<GameObject>(sFilePath);
-        goScreen.GetComponent<CanvasGroup>().alpha = 0;
+        CanvasGroup cg = goScreen.GetComponent<CanvasGroup>();
+        cg.alpha = 0;
+        cg.blocksRaycasts = false;
+        cg.interactable = false;
+        Canvas c = goScreen.GetComponent<Canvas>();
+        c.worldCamera = Camera.main;
+        c.planeDistance = 5;
         GameObject goEditorObject = Instantiate(goScreen, new Vector3(0, 0, -3), Quaternion.identity);
         StartCoroutine(FadeUI(goEditorObject.GetComponent<CanvasGroup>(), '+'));
     }
