db91a98126da93137faf2c593ce7c46c0742c322
Tate Smith <tate.smith.4242@gmail.com>
2020-05-18 18:40:13 -0700

    added leaderboard functionality

---
+++ Lockdown Simulator/Assets/Scripts/FirebaseInit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Firebase;
+using Firebase.Extensions;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FirebaseInit : MonoBehaviour {
+    private static FirebaseInit _instance;
+
+    public UnityEvent OnFirebaseInitialized = new UnityEvent();
+
+    void Awake() {
+        if (_instance != null && _instance != this) {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
+            if (task.Exception != null) {
+                Debug.LogError($"Failed to initialize Firebase with error {task.Exception}");
+                return;
+            }
+
+            FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://lockdown-simulator.firebaseio.com/");
+
+            OnFirebaseInitialized.Invoke();
+        });
+    }
+}
