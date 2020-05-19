ac9d1c4bbb48888a9b14809b0c79ab0fc6038d8c
Jacob Tan <jacobmathewtan@gmail.com>
2020-05-19 15:11:08 -0700

    Finished leaderboard

--- docs/scripts/firebase.js
+++ docs/scripts/firebase.js
@@ -14,67 +14,19 @@ const db = firebase.database();
 let unsortedUsers = [];
 let sortedUsers = [];
 
-function grabUserData() {
-    let users = db.ref('Players').orderByKey();
-    users.once("value")
-        .then(function (snapshot) {
-            snapshot.forEach(function (childSnapshot) {
-                let key = childSnapshot.key;
-                //console.log(key); //userid
-                let data = childSnapshot.val();
-                console.log(data);
-                //console.log(data); //user score and username
-                unsortedUsers.push(data);
-            })
+function printUsers() {
+    let rootRef = db.ref().orderByChild("score");
+    rootRef.on('value', function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+            let parent = document.getElementById("end").parentNode;
+            let childData = childSnapshot.val();
+            let score = childData["name"] + " ----- " + childData["score"];
+            let node = document.createTextNode(score);
+            let h4 = document.createElement("h4");
+            h4.append(node);
+            parent.prepend(h4);
         });
-    console.log("abc");
-    console.log(unsortedUsers);
-    sortedUsers = unsortedUsers.sort(function (a, b) {
-        console.log("abcd");
-        console.log(a.score);
-        return a.score - b.score;
     });
 }
 
-grabUserData();
-console.log(unsortedUsers);
-console.log(unsortedUsers[0]);
-
-
-
-
-////////////////////////////////////////////////
-//Test function for writing data into database
-function writeUserData(userId, name, score) {
-    db.ref('Players/' + userId).set({
-        username: name,
-        score: score
-    });
-}
-/**
-writeUserData("player1", "bob", 20);
-writeUserData("player2", "dylan", 30);
-writeUserData("player3", "jon", 7);
-*/
-////////////////////////////////////////////////
-
-
-/**
- * Show leaderboard
- 
-function showLeaderboard() {
-    let docRef = db.collection("Players");
-    docRef.orderBy("score", "desc").limit(10)
-        .get()
-        .then(function (querySnapshot) {
-            querySnapshot.forEach(function (doc) {
-                let score = doc.data().name + "-----" + doc.data().score;
-                let p = document.createElement("p");
-                let node = document.createTextNode(score);
-                p.appendChild(node);
-                document.body.appendChild(p);
-            });
-        });
-}
-showLeaderboard();
-*/
+printUsers();
